8f96059731c55d886381d3c37b4de4492ea401b9
USAMI Kosuke <usami.kosuke@gmail.com>
2023-11-03 00:37:31 +0900

    spiral

---
+++ 2023/0006_spiral/src/main.rs
@@ -0,0 +1,66 @@
+use nannou::color::*;
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+struct Model {
+    _window: window::Id,
+    start_animation: bool,
+    start: Point2,
+    end: Point2,
+    angle: f32,
+    radius: f32,
+}
+
+fn model(app: &App) -> Model {
+    let _window = app
+        .new_window()
+        .size(600, 600)
+        .key_pressed(key_pressed)
+        .view(view)
+        .build()
+        .unwrap();
+    Model {
+        _window,
+        start_animation: false,
+        start: pt2(0.0, 0.0),
+        end: pt2(0.0, 0.0),
+        angle: 0.0,
+        radius: 0.0,
+    }
+}
+
+fn key_pressed(_app: &App, model: &mut Model, key: Key) {
+    if key == Key::Space {
+        model.start_animation = !model.start_animation
+    }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    if !model.start_animation {
+        return;
+    }
+
+    model.start = model.end;
+    model.angle += PI / 30.0;
+    model.radius += 1.1.pow(model.angle) as f32 / 60.0;
+    model.end = pt2(model.angle.cos(), model.angle.sin()) * model.radius;
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    if app.elapsed_frames() == 0 {
+        draw.background().color(WHITE);
+    }
+
+    draw.line()
+        .start(model.start)
+        .end(model.end)
+        .color(STEELBLUE)
+        .weight(2.0);
+
+    draw.to_frame(app, &frame).unwrap();
+}
